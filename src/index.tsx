2bd9357748ff936d6b8e788e789149c74759cb76
Fzgt <vermouth7z@foxmail.com>
2023-08-15 12:50:46 +0800

    chore: delelte annotation

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import React from 'react';
-import './index.css';
+// import './index.css';
 import { createRoot } from 'react-dom/client';
 import App from '@pages/App';
 
--- src/pages/TestPages/ToDoList.tsx
+++ src/pages/TestPages/ToDoList.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './ToDoList.css';
 
 const ToDoList = () => {
-  return;
-  <>
-    <div className="list-wrapper">
-      <h1>TO DO LIST</h1>
-      <input className="text" type="text" />
-    </div>
-  </>;
+  return (
+    <>
+      <div className="list-wrapper">
+        <h1>TO DO LIST</h1>
+        <input className="text" type="text" />
+      </div>
+    </>
+  );
 };
 
 export default ToDoList;
